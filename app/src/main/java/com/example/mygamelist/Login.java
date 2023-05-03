67c0121c4b3ec839aff8c2c90d3b523aba772acd
Mikel <mlopezv07@educantabria.es>
2023-05-03 22:24:35 +0200

    Todas las interfaces planas y navegacion entre login y registro, y login y menu principal hechas y funcionales

--- app/src/main/java/com/example/mygamelist/Login.java
+++ app/src/main/java/com/example/mygamelist/Login.java
@@ -2,13 +2,42 @@ package com.example.mygamelist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        Button botonRegistrar = findViewById(R.id.boton_sign);
+        Button botonLogin = findViewById(R.id.boton_login);
+        botonRegistrar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Crear un objeto Intent para la actividad de destino
+                Intent intent = new Intent(Login.this, Registro.class);
+
+                // Iniciar la actividad de destino
+                startActivity(intent);
+            }
+        });
+
+
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Crear un objeto Intent para la actividad de destino
+                Intent intent = new Intent(Login.this, MenuPrincipal.class);
+
+                // Iniciar la actividad de destino
+                startActivity(intent);
+            }
+        });
+
     }
 }
