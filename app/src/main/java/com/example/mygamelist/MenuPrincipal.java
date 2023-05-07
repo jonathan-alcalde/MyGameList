830c16c557bae5249d5f9bca208c8f442cf9a843
Mikel <mlopezv07@educantabria.es>
2023-05-07 22:02:33 +0200

    menu principal, grid view con 21 imagenes de prueba cargadas con exito, scrolleable y con buen tamaño y espaciado

--- app/src/main/java/com/example/mygamelist/MenuPrincipal.java
+++ app/src/main/java/com/example/mygamelist/MenuPrincipal.java
@@ -1,10 +1,13 @@
 package com.example.mygamelist;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
@@ -15,31 +18,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuPrincipal extends AppCompatActivity {
 
-    private Integer[] imageResources = {
-            R.drawable.j1,
-            R.drawable.j2,
-            R.drawable.j3,
-            R.drawable.j4,
-            R.drawable.j5,
-            R.drawable.j6,
-            R.drawable.j7,
-            R.drawable.j8,
-            R.drawable.j9,
-            R.drawable.j10,
-            R.drawable.j11,
-            R.drawable.j12,
-            R.drawable.j13,
-            R.drawable.j14,
-            R.drawable.j15,
-            R.drawable.j16,
-            R.drawable.j17,
-            R.drawable.j18,
-            R.drawable.j19,
-            R.drawable.j20,
-            R.drawable.j21
-    };
-
     private GridView gridView;
+    private ImageAdapter adapter;
+    private int[] images = {R.drawable.j1, R.drawable.j2, R.drawable.j3, R.drawable.j4, R.drawable.j5, R.drawable.j6, R.drawable.j7, R.drawable.j8, R.drawable.j9, R.drawable.j10, R.drawable.j11, R.drawable.j12, R.drawable.j13, R.drawable.j14, R.drawable.j15, R.drawable.j16, R.drawable.j17, R.drawable.j18, R.drawable.j19, R.drawable.j20, R.drawable.j21};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,23 +28,39 @@ public class MenuPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_menu_principal);
 
         gridView = findViewById(R.id.grid_view);
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(
-                this,
-                android.R.layout.simple_list_item_1,
-                imageResources
-        ) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                ImageView imageView = new ImageView(getContext());
-                imageView.setImageResource(imageResources[position]);
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setLayoutParams(new GridView.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        350
-                ));
-                return imageView;
-            }
-        };
+
+        adapter = new ImageAdapter();
         gridView.setAdapter(adapter);
+    }
+
+    class ImageAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(MenuPrincipal.this).inflate(R.layout.grid_item, parent, false);
+            }
+
+            ImageView imageView = convertView.findViewById(R.id.imageView);
+
+            imageView.setImageResource(images[position]);
+
+            return convertView;
+        }
     }
 }
